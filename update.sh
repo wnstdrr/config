18a145e4aa4cb06f15041b0f89fdb5db25d54034
xtedder7 <xandertedder7@gmail.com>
2022-05-08 10:32:34 -0400

    add update.sh

---
+++ update.sh
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#!/bin/bash
+
+$PLUGIN_PATH="$HOME/.urxvt/ext/"
+$RXVT_INSTALLED=0
+
+# Check if rxvt-unicode is installed if not install it
+if [[ $(which rxvt-unicode) == "/usr/bin/rxvt-unicode" ]]; then
+    $RXVT_INSTALLED=1
+fi
+
+if [[ $RXVT_INSTALLED -eq 0 ]]; then
+    sudo apt install rxvt-unicode
+elif [[ $RXVT_INSTALLED -eq 1 ]]; then
+    echo "rxvt-unicode installed at $(which rxvt-unicode)"
+fi
+
+
+if [[ ! -e $PLUGIN_PATH ]]; then
+    # Create plugin directory
+    mkdir $PLUGIN_PATH
+fi
+cp plugins/vtmouse $PLUGIN_PATH
+cp .vimrc $HOME
+
+# Copy .Xresources to $HOME & update
+cp .Xresources $HOME
+xrdb $HOME/.Xresources
