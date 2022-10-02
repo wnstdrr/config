1d0b55efb6de527a17ddf1c39419d85f5c004185
Xander I. Tedder <xtedder@slingshotedu.com>
2022-10-02 22:54:44 +0000

    add package, comments

--- dotfiles/void-packages.sh
+++ dotfiles/void-packages.sh
@@ -2,12 +2,16 @@
 
 # Collection of packages needed for void linux
 # to start i3-gaps, feh, compton/picom, etc.
+#
+# Other QOL apps to make the desktop enviroment
+# look better than it should.
 
 sudo xbps-install -Syu
 sudo xbps-install -Sy xbps
 sudo xbps-install -Sy \
-    i3-gaps \
-    xorg \
-    feh \
-    compton \
-    dmenu \
+    i3-gaps \  # Window Manager
+    xorg \     # xorg/x11 server
+    feh \      # Desktop Backgrounds
+    compton \  # Window Composition
+    dmenu \    # Interactive Menu
+    glava \    # Desktop Audio Visualization
